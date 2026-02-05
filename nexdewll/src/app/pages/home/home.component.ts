41838791aa8a4e0a3af92826f7185dad56789f0b
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-06 00:23:52 +0530

    Added property search section

--- nexdewll/src/app/pages/home/home.component.ts
+++ nexdewll/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { HeroComponent } from "../../components/hero/hero.component";
+import { PropertySearchComponent } from "../../components/property-search/property-search.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, HeroComponent],
+  imports: [NavBarComponent, HeroComponent, PropertySearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
