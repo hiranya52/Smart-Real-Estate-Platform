13b6f5dc1e1b8a54eb2537bfe44cc46713fdd88d
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-06 00:32:42 +0530

    Add properties section with property cards

--- nexdewll/src/app/pages/home/home.component.ts
+++ nexdewll/src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { HeroComponent } from "../../components/hero/hero.component";
 import { PropertySearchComponent } from "../../components/property-search/property-search.component";
+import { NewPropertiesComponent } from "../../components/new-properties/new-properties.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, HeroComponent, PropertySearchComponent],
+  imports: [NavBarComponent, HeroComponent, PropertySearchComponent, NewPropertiesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
